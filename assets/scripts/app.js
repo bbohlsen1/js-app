be568fbf99a5231a579a8069a8b2c48ede318e90
Brett <bdbohlsen1@gmail.com>
2025-10-29 22:30:56 -0500

    Import and export basics

---
+++ assets/scripts/app.js
@@ -0,0 +1,11 @@
+//import { apiKey } from "./util.js";
+
+
+//import apiKey from "./util.js";
+
+import { apiKey, abc as content } from "./util.js"
+
+//import * as util from "./util.js";
+
+//console.log(apiKey);
+console.log(content);
---
+++ assets/scripts/util.js
@@ -0,0 +1,4 @@
+//export let apiKey = "asdlfkjslgj356";
+export default "assdlfksdfjkl";
+export let apiKey = "asldkfj";
+export let abc = "abc";
